3ecb34ccd70c460244e865efa3420ed510aba7ce
Michael Tsai <huanlin.tsai@gmail.com>
2018-05-25 06:57:58 +0800

    Resolved #27. 在雙視編輯模式下提供複製、貼上功能。

--- Source/EasyBrailleEdit/DualEdit/ClipboardHelper.cs
+++ Source/EasyBrailleEdit/DualEdit/ClipboardHelper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BrailleToolkit;
+using Huanlin.Common.Helpers;
 using SourceGrid;
 
 namespace EasyBrailleEdit.DualEdit
@@ -16,8 +17,13 @@ namespace EasyBrailleEdit.DualEdit
 
         public static void SetData(List<BrailleWord> words)
         {
+            var s = JsonHelper.Serialize(words);
+
             Clipboard.Clear();
-            Clipboard.SetData(ClipboardObjectFormat, words);
+            Clipboard.SetData(ClipboardObjectFormat, s);
+
+            // 注意：這裡不使用 Clipboard 內建的序列化，是因為它會遺漏 BrailleWord 的 PhoneticCode 屬性
+            //Clipboard.SetData(ClipboardObjectFormat, words);
         }
 
         public static List<BrailleWord> GetData()
@@ -25,7 +31,8 @@ namespace EasyBrailleEdit.DualEdit
             List<BrailleWord> result = null;
             if (Clipboard.ContainsData(ClipboardObjectFormat))
             {
-                result = (List<BrailleWord>) Clipboard.GetData(ClipboardObjectFormat);
+                var s = (string) Clipboard.GetData(ClipboardObjectFormat);
+                result = JsonHelper.Deserialize<List<BrailleWord>>(s);
             }
             return result;
         }
