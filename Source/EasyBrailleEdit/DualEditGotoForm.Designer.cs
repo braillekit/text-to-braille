36c6a59e16642ecd8ed6d07565bc5494e46bac56
Michael Tsai <huanlin.tsai@gmail.com>
2018-06-05 14:00:17 +0800

    改善 Goto 對話窗的 UI。

--- Source/EasyBrailleEdit/DualEditGotoForm.Designer.cs
+++ Source/EasyBrailleEdit/DualEditGotoForm.Designer.cs
@@ -33,6 +33,7 @@
             this.numPosition = new System.Windows.Forms.NumericUpDown();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numPosition)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,10 +41,10 @@
             // 
             this.rdoLine.AutoSize = true;
             this.rdoLine.Checked = true;
-            this.rdoLine.Location = new System.Drawing.Point(25, 25);
+            this.rdoLine.Location = new System.Drawing.Point(141, 30);
             this.rdoLine.Name = "rdoLine";
             this.rdoLine.Size = new System.Drawing.Size(77, 23);
-            this.rdoLine.TabIndex = 0;
+            this.rdoLine.TabIndex = 1;
             this.rdoLine.TabStop = true;
             this.rdoLine.Text = "列 (&L)";
             this.rdoLine.UseVisualStyleBackColor = true;
@@ -51,16 +52,17 @@
             // rdoPage
             // 
             this.rdoPage.AutoSize = true;
-            this.rdoPage.Location = new System.Drawing.Point(26, 50);
+            this.rdoPage.Location = new System.Drawing.Point(234, 30);
             this.rdoPage.Name = "rdoPage";
             this.rdoPage.Size = new System.Drawing.Size(76, 23);
-            this.rdoPage.TabIndex = 1;
+            this.rdoPage.TabIndex = 2;
             this.rdoPage.Text = "頁 (&P)";
             this.rdoPage.UseVisualStyleBackColor = true;
             // 
             // numPosition
             // 
-            this.numPosition.Location = new System.Drawing.Point(26, 84);
+            this.numPosition.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.numPosition.Location = new System.Drawing.Point(46, 26);
             this.numPosition.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -73,7 +75,7 @@
             0});
             this.numPosition.Name = "numPosition";
             this.numPosition.Size = new System.Drawing.Size(79, 30);
-            this.numPosition.TabIndex = 2;
+            this.numPosition.TabIndex = 0;
             this.numPosition.Value = new decimal(new int[] {
             1,
             0,
@@ -82,29 +84,43 @@
             // 
             // btnOk
             // 
+            this.btnOk.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOk.Location = new System.Drawing.Point(156, 25);
+            this.btnOk.Location = new System.Drawing.Point(87, 91);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 30);
+            this.btnOk.Size = new System.Drawing.Size(81, 43);
             this.btnOk.TabIndex = 3;
             this.btnOk.Text = "確定";
             this.btnOk.UseVisualStyleBackColor = true;
             // 
             // btnCancel
             // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(156, 61);
+            this.btnCancel.Location = new System.Drawing.Point(174, 91);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 30);
+            this.btnCancel.Size = new System.Drawing.Size(84, 43);
             this.btnCancel.TabIndex = 4;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "第";
+            // 
             // DualEditGotoForm
             // 
+            this.AcceptButton = this.btnOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 130);
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(341, 146);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.numPosition);
@@ -132,5 +148,6 @@
 		private System.Windows.Forms.NumericUpDown numPosition;
 		private System.Windows.Forms.Button btnOk;
 		private System.Windows.Forms.Button btnCancel;
-	}
+        private System.Windows.Forms.Label label1;
+    }
 }
