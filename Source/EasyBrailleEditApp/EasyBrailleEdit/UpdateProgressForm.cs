db11b0571694a839a0e20444b6e1dc41d139fc20
huanlin <huanlin.tsai@gmail.com>
2025-11-15 22:24:27 +0800

    Resolved #37 (非同步呼叫與更新 UI 的問題）

--- Source/EasyBrailleEditApp/EasyBrailleEdit/UpdateProgressForm.cs
+++ Source/EasyBrailleEditApp/EasyBrailleEdit/UpdateProgressForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http.Handlers;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Huanlin.Common.Http;
 
@@ -19,27 +20,39 @@ namespace EasyBrailleEdit
             txtMsg.Clear();
         }
 
-        public void updator_FileUpdating(object sender, HttpUpdaterFileEventArgs args)
+        public async void updator_FileUpdating(object sender, HttpUpdaterFileEventArgs args)
         {
-            txtMsg.AppendText("正在更新 " + args.FileName);
-            txtMsg.AppendText(String.Format(" (第 {0} 個，共 {1} 個) ....", args.Number, args.Total));
+            await txtMsg.InvokeAsync(() =>
+            {
+                txtMsg.AppendText("正在更新 " + args.FileName);
+                txtMsg.AppendText(String.Format(" (第 {0} 個，共 {1} 個) ....", args.Number, args.Total));
+            });
         }
 
-        public void updator_FileUpdated(object sender, HttpUpdaterFileEventArgs args)
+        public async void updator_FileUpdated(object sender, HttpUpdaterFileEventArgs args)
         {
-            txtMsg.AppendText(" 完成!");
-            txtMsg.AppendText(Environment.NewLine);
+            await txtMsg.InvokeAsync(() =>
+            {
+                txtMsg.AppendText(" 完成!");
+                txtMsg.AppendText(Environment.NewLine);
+            });
         }
 
 
-        public void updator_DownloadProgressChanged(object sender, HttpProgressEventArgs e)
+        public async void updator_DownloadProgressChanged(object sender, HttpProgressEventArgs e)
         {
-            progressBar1.Value = e.ProgressPercentage;
+            await progressBar1.InvokeAsync(() =>
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            });
         }
 
-        public void AppendMessage(string msg)
+        public async Task AppendMessage(string msg)
         {
-            txtMsg.AppendText(msg);
+            await txtMsg.InvokeAsync(() =>
+            {
+                txtMsg.AppendText(msg);
+            });
         }
 
     }
