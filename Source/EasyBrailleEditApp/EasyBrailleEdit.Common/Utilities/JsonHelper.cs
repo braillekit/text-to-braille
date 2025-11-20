26bf6ba5a63d0692db28749927db74bb37986157
Michael Tsai <huanlin.tsai@gmail.com>
2025-11-20 21:32:56 +0800

    加上註解提醒。

--- Source/EasyBrailleEditApp/EasyBrailleEdit.Common/Utilities/JsonHelper.cs
+++ Source/EasyBrailleEditApp/EasyBrailleEdit.Common/Utilities/JsonHelper.cs
@@ -3,6 +3,10 @@ using System.Text;
 
 namespace EasyBrailleEdit.Common.Utilities
 {
+    /// <summary>
+    /// JSON 序列化/反序列化輔助類別。
+    /// 注意：不可將 DataContractJsonSerializer 改為 System.Text.Json，否則會引發一連串的序列化／反序列化的問題。
+    /// </summary>
     public class JsonHelper
     {
         public static string Serialize<T>(T obj)
@@ -17,7 +21,7 @@ namespace EasyBrailleEdit.Common.Utilities
 
         public static T Deserialize<T>(string jsonStr)
         {
-            T obj = Activator.CreateInstance<T>();  // �`�N: ���ϧǦC�ƪ����O�������w�]�غc��.
+            T obj = Activator.CreateInstance<T>();  // 注意: 欲反序列化的類別必須有預設建構元.
             using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonStr)))
             {
                 DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
